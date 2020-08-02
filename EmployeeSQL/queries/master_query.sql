85bae025b1c6f77db3d91e225408cf76851e2826
gitgrant <63924944+gitgranthub@users.noreply.github.com>
2020-08-02 16:16:10 -0500

    problem 1 solve

--- EmployeeSQL/queries/master_query.sql
+++ EmployeeSQL/queries/master_query.sql
@@ -5,3 +5,11 @@ SELECT * FROM dept_manager;
 SELECT * FROM employees;
 SELECT * FROM salaries;
 SELECT * FROM titles;
+
+-- 1. List the following details of each employee: 
+--employee number, last name, first name, sex, and salary.
+
+SELECT employees.emp_no, employees.last_name, employees.first_name, employees.sex, salaries.salary
+FROM salaries
+INNER JOIN employees ON
+employees.emp_no = salaries.emp_no;
