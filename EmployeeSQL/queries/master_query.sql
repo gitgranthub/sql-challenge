df1210d2f8d0011371b7f68d740572426af7d2b2
gitgrant <63924944+gitgranthub@users.noreply.github.com>
2020-08-02 15:06:42 -0500

    created diagram, tables and imported csv files

---
+++ EmployeeSQL/queries/master_query.sql
@@ -0,0 +1,7 @@
+-- CHECK TABLES AFTER IMPORT AND FOR INTEGRITY
+SELECT * FROM departments;
+SELECT * FROM dept_emp;
+SELECT * FROM dept_manager;
+SELECT * FROM employees;
+SELECT * FROM salaries;
+SELECT * FROM titles;
