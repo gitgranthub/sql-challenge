6699f6e5af54a52ba0b82e77f56b17ca9413be3f
gitgrant <63924944+gitgranthub@users.noreply.github.com>
2020-08-02 20:48:22 -0500

    problem 5 solved

--- EmployeeSQL/queries/master_query.sql
+++ EmployeeSQL/queries/master_query.sql
@@ -41,3 +41,10 @@ FROM employees AS e
     JOIN departments AS d
     ON (d.dept_no = p.dept_no)
 	ORDER BY e.emp_no ASC;
+	
+--List first name, last name, and sex 
+--for employees whose first name is "Hercules" and last names begin with "B."
+
+SELECT e.first_name, e.last_name, e.sex  
+FROM employees AS e
+WHERE e.first_name LIKE 'Hercules' AND e.last_name LIKE 'B%';
